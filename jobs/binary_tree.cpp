220b3cced7dcba29faeee70c5a9c13c3c8c72013
syz <1448510231@qq.com>
2023-04-21 01:02:30 +0800

    update binary tree

--- jobs/binary_tree.cpp
+++ jobs/binary_tree.cpp
@@ -65,3 +65,70 @@ void leveltraverse(TreeNode* root) {
         }
     }
 }
+
+// 100 same tree
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (p == nullptr && q == nullptr) return true;
+    if (p == nullptr || q == nullptr) return false;
+    if (p->val != q->val) return false;
+    if (isSameTree(p->left, q->left) && 
+        isSameTree(p->right, q->right)) {
+            return true;
+        }
+    return false;
+}
+
+
+// 700  二叉搜索树中的搜索
+TreeNode* searchNode(TreeNode* root, int val) {
+    TreeNode* node = nullptr;
+    if (root == nullptr || root->val == val) {
+        return root;
+    }
+    if (root->val < val) {
+        node = searchNode(root->right, val);
+    }
+    if (root->val > val) {
+        node = searchNode(root->left, val);
+    }
+    return node;
+}
+
+// 450 删除二叉搜索树中的节点
+
+TreeNode* deleteNode(TreeNode* root, int val) {
+    if (root == nullptr) return nullptr;
+    if (val < root->val) {
+        root->left = deleteNode(root->left, val);
+    } else (val > root->val) {
+        root->right = deleteNode(root->right, val);
+    } else {
+        if (root->left == nullptr) {
+            return root->right;
+        } else if (root->right == nullptr) {
+            return root->left;
+        } else {
+            TreeNode* node = root->right;
+            while (node->left != nullptr) {
+                node = node->left;
+            }
+            node->left = root->left;
+            return root->right;
+        }
+    }
+    return root;
+}
+
+// 701 二叉搜索树中的插入操作
+TreeNode* insertIntoBST(TreeNode* root, int val) {
+    if (root == nullptr) {
+        return new TreeNode(val);
+    }
+    if (root->val > val) {
+        root->left = insertIntoBST(root->left, val);
+    }
+    if (root->val < val) {
+        root->right = insertIntoBST(root->right, val);
+    }
+    return root;
+}
